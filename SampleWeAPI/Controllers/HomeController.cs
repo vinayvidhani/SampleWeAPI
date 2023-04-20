6a2bfd9a1244767d082bf541c48fea60d292124d
Vinay-Coditas <vinay.vidhani@coditas.com>
2023-04-20 11:56:04 +0530

    Initial setup with get api

---
+++ SampleWeAPI/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly List<Person> _persons = new List<Person>()
+        {
+            new Person() { Id= 1, Name = "Vinay"} ,
+            new Person() { Id= 2, Name = "Hridya"},
+            new Person() { Id= 3, Name = "Prisha"},
+            new Person() { Id= 4, Name = "Kavita"}
+        };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<Person> Get()
+        {
+            return _persons;
+        }
+    }
+}
---
+++ SampleWeAPI/Person.cs
@@ -0,0 +1,8 @@
+namespace SampleWeAPI
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
